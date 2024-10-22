bd2d4bb78b5d95914d3bb138c659871da3d112db
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-10-22 17:46:20 -0500

    adds tests

--- unit_test.py
+++ unit_test.py
@@ -1,7 +1,9 @@
 import math
 import unittest
-
-
+import global_game_data
+from pathing import get_bfs_path, get_dfs_path
+import graph_data
+import pyglet
 class TestPathFinding(unittest.TestCase):
 
     def test_upper(self):
@@ -23,6 +25,28 @@ class TestPathFinding(unittest.TestCase):
         self.assertNotEqual(almost_pi, pi)
         self.assertAlmostEqual(first=almost_pi, second=pi, delta=1e-1)
 
+class TestBFS(unittest.TestCase):
+    def test_bfs1(self):
+        path = get_bfs_path()
+        self.assertEqual(path, [0,1,2])
+        pass
 
+    def test_dfs1(self):
+        path = get_bfs_path()
+        self.assertEqual(path, [0,1,2])
+        pass
+    
+    def test_bfs2(self):
+        global_game_data.current_graph_index = 1
+        path = get_bfs_path()
+        self.assertEqual(path, [0,1,2,3])
+        pass
+
+    def test_dfs2(self):
+        global_game_data.current_graph_index = 1
+        path = get_dfs_path()
+        self.assertEqual(path, [0,1,2,3])
+        pass
 if __name__ == '__main__':
+    pyglet.app.run()
     unittest.main()
